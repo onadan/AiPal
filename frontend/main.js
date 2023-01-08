69e3299a64a25adfe71e66a07ff77c7f09ca703f
onadan <onadan45@gmail.com>
2023-01-08 21:22:47 +0100

    added vercel.json

--- frontend/main.js
+++ frontend/main.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chat-gpt-clone-fmxc.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
